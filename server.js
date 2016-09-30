31f23ad019ce6bfaac5b80c625427873d442ee39
hkataria11 <hkataria11@gmail.com>
2016-10-01 01:01:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.get('/', function (req, res) {
 });
 
 app.get('/Article-one',function(req,res){
-    res.send("Article-three requested and will be served here");
+    res.send("Article-one requested and will be served here");
 });
 
 app.get('/Article-two',function(req,res){
-    res.send("Article-three requested and will be served here");
+    res.send("Article-two requested and will be served here");
 });
 
 app.get('/Article-three',function(req,res){
