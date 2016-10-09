18de4fce562e97a4f50f00e0f2b4404b5d6d01dd
hkataria11 <hkataria11@gmail.com>
2016-10-09 12:31:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,11 @@ element.innerHTML='Hi! I am Harshit and this is my  webapp';
 var img= document.getElementById('madi');
 maginLeft=0;
 function moveRight(){
-    marginLeft= marginLeft +10;
+    marginLeft= marginLeft +1;
     img.syle.marginLeft= marginLeft+'px';
 }
 img.onclick = function(){
-   var interval = setInterval(moveRight,100);
-   img.style.marginLeft='100px'; 
+   var interval = setInterval(moveRight,50);
+ 
    
 };
