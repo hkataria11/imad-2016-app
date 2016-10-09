935bdbcafa39c69a29bb7d99ffc797d9395ae8d3
hkataria11 <hkataria11@gmail.com>
2016-10-09 12:24:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById('main-txt');
 element.innerHTML='Hi! I am Harshit and this is my  webapp';
-var img= document.getElementById('img');
+var img= document.getElementById('madi');
 img.onclick = function(){
    img.style.marginLeft='100px'; 
 };
