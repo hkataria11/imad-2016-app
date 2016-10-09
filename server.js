43ea97898a5d4e07d8191096cea25f7b728e3ffa
hkataria11 <hkataria11@gmail.com>
2016-10-09 19:33:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,14 +100,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+ var counter=0;
+app.get('/counter',function(req,res){
+    counter = counter + 
+    res.send(counter.toString());
+});
+
+
 app.get('/:ArticleName',function(req,res){
     var ArticleName= req.params.ArticleName;
    res.send(createTemplate(Articles[ArticleName]));
-});
- var counter=0;
-app.get('/counter',function(req,res){
-    counter = counter + 1;
-    res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {
