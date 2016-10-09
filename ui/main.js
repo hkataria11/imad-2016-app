2f707157a34a5fc1de115a6807651a57521ba218
hkataria11 <hkataria11@gmail.com>
2016-10-09 20:16:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,18 @@
 var button = document.getElementById('counter');
-var counter=0;
+
 
 button.onclick= function(){
-    counter=counter+1;
+
+var request = new XMLHttpRequest();
+request.onreadystatechange = function(){
+if(request.readyState === XMLHttpRequest.DONE){
+if(request.status === 200){
 var span =document.getElementById('count');
 span.innerHTML =  counter.toString();
-    
-    
+
+}
+}
+};
+request.open('GET','http:hkataria11.imad.hasura-app.io/counter',true);
+request.send(null);
 };
